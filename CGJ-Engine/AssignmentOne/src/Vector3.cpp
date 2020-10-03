802cd667da1f52eaba273bbfeb171a322d88d205
HerouFenix <diogo04@ua.pt>
2020-10-03 18:50:21 +0100

    Added getters/setters

--- CGJ-Engine/AssignmentOne/src/Vector3.cpp
+++ CGJ-Engine/AssignmentOne/src/Vector3.cpp
@@ -13,8 +13,45 @@ Vector3::Vector3(float _x, float _y, float _z) {
     z = _z;
 }
 
+//Vector3 Print
+
 std::ostream& operator<<(std::ostream& os, const Vector3& vec)
 {
     os << '(' << vec.x << ',' << vec.y << ',' << vec.z << ')';
     return os;
 }
+
+//Vector3 Getters
+
+float Vector3::getX() {
+    return x;
+}
+
+float Vector3::getY() {
+    return y;
+}
+
+float Vector3::getZ() {
+    return z;
+}
+
+//Vector3 Setters
+
+void Vector3::setX(float _x) {
+    x = _x;
+}
+
+void Vector3::setY(float _y) {
+    y = _y;  
+}
+
+void Vector3::setZ(float _z) {
+    z = _z;
+}
+
+
+//Vector3 Operations
+
+//Vector3 Assignments
+
+//Vector3 Methods
--- CGJ-Engine/AssignmentOne/src/main.cpp
+++ CGJ-Engine/AssignmentOne/src/main.cpp
@@ -4,9 +4,20 @@
 int main() {
     Vector3 vec1(1,2,3);
     std::cout << "Vector 1: " << vec1 << "\n";
+    std::cout << "  x: " << vec1.getX() << "\n";
+    std::cout << "  y: " << vec1.getY() << "\n";
+    std::cout << "  z: " << vec1.getZ() << "\n";
 
     Vector3 vec2;
     std::cout << "Vector 2: " << vec2 << "\n";
+    vec2.setX(3);
+    vec2.setY(2);
+    vec2.setZ(1);
+
+    std::cout << "Vector 2 after set:\n";
+    std::cout << "  x: " << vec2.getX() << "\n";
+    std::cout << "  y: " << vec2.getY() << "\n";
+    std::cout << "  z: " << vec2.getZ() << "\n";
 
     return 0;
 }
