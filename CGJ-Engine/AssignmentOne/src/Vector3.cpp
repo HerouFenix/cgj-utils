ae66dbd90cf6b872127fcb54cbab575b5fa6c66d
HerouFenix <diogo04@ua.pt>
2020-10-04 00:02:26 +0100

    added titles to the vector test main

--- CGJ-Engine/AssignmentOne/src/Vector3.cpp
+++ CGJ-Engine/AssignmentOne/src/Vector3.cpp
@@ -3,146 +3,146 @@
 
 // Vector 3 Constructors
 Vector3::Vector3() {
-    x = 0;
-    y = 0;
-    z = 0;
+	x = 0;
+	y = 0;
+	z = 0;
 }
 
 Vector3::Vector3(float _x, float _y, float _z) {
-    x = _x;
-    y = _y;
-    z = _z;
+	x = _x;
+	y = _y;
+	z = _z;
 }
 
 //Vector3 Print
 
 std::ostream& operator<<(std::ostream& os, const Vector3& vec)
 {
-    os << '(' << vec.x << ',' << vec.y << ',' << vec.z << ')';
-    return os;
+	os << '(' << vec.x << ',' << vec.y << ',' << vec.z << ')';
+	return os;
 }
 
 //Vector3 Getters
 
 float Vector3::getX() {
-    return x;
+	return x;
 }
 
 float Vector3::getY() {
-    return y;
+	return y;
 }
 
 float Vector3::getZ() {
-    return z;
+	return z;
 }
 
 //Vector3 Setters
 
 void Vector3::setX(float _x) {
-    x = _x;
+	x = _x;
 }
 
 void Vector3::setY(float _y) {
-    y = _y;  
+	y = _y;
 }
 
 void Vector3::setZ(float _z) {
-    z = _z;
+	z = _z;
 }
 
 
 //Vector3 Operations
 float Vector3::magnitude() {
-    return sqrt((x * x) + (y * y) + (z * z));
+	return sqrt((x * x) + (y * y) + (z * z));
 }
 
 //Vector3 Assignments
 
 //Vector3 Methods
 
-    //addition
-    Vector3 Vector3::operator+(const Vector3& operand)
-    {
-        return Vector3(x + operand.x, y + operand.y, z + operand.z);
-    }
-    Vector3& Vector3::operator+=(const Vector3& operand)
-    {
-        x += operand.x;
-        y += operand.y;
-        z += operand.z;
-        return *this;
-    }
+	//addition
+Vector3 Vector3::operator+(const Vector3& operand)
+{
+	return Vector3(x + operand.x, y + operand.y, z + operand.z);
+}
+Vector3& Vector3::operator+=(const Vector3& operand)
+{
+	x += operand.x;
+	y += operand.y;
+	z += operand.z;
+	return *this;
+}
 
-    //subtraction
-    Vector3 Vector3::operator-(const Vector3& operand)
-    {
-        return Vector3(x - operand.x, y - operand.y, z - operand.z);
-    }
+//subtraction
+Vector3 Vector3::operator-(const Vector3& operand)
+{
+	return Vector3(x - operand.x, y - operand.y, z - operand.z);
+}
 
-    Vector3& Vector3::operator-=(const Vector3& operand)
-    {
-        x -= operand.x;
-        y -= operand.y;
-        z -= operand.z;
-        return *this;
-    }
+Vector3& Vector3::operator-=(const Vector3& operand)
+{
+	x -= operand.x;
+	y -= operand.y;
+	z -= operand.z;
+	return *this;
+}
 
-    //multiplication by scalar
-    Vector3 Vector3::operator*(float operand)
-    {
-        return Vector3(x * operand, y * operand, z * operand);
-    }
-    Vector3& Vector3::operator*=(float operand)
-    {
-        x *= operand;
-        y *= operand;
-        z *= operand;
-        return *this;
-    }
+//multiplication by scalar
+Vector3 Vector3::operator*(float operand)
+{
+	return Vector3(x * operand, y * operand, z * operand);
+}
+Vector3& Vector3::operator*=(float operand)
+{
+	x *= operand;
+	y *= operand;
+	z *= operand;
+	return *this;
+}
 
-    //Division by scalar
-    Vector3 Vector3::operator/(float operand)
-    {
-        assert(operand != 0);
-        return Vector3(x / operand, y / operand, z / operand);
-    }
+//Division by scalar
+Vector3 Vector3::operator/(float operand)
+{
+	assert(operand != 0);
+	return Vector3(x / operand, y / operand, z / operand);
+}
 
-    Vector3& Vector3::operator/=(float operand)
-    {
-        x /= operand;
-        y /= operand;
-        z /= operand;
-        return *this;
-    }
+Vector3& Vector3::operator/=(float operand)
+{
+	x /= operand;
+	y /= operand;
+	z /= operand;
+	return *this;
+}
 
-    //Vector comparison
-    Vector3& Vector3::operator=(const Vector3& operand)
-    {
-        x = operand.x;
-        y = operand.y;
-        z = operand.z;
-        return *this;
-    }
+//Vector comparison
+Vector3& Vector3::operator=(const Vector3& operand)
+{
+	x = operand.x;
+	y = operand.y;
+	z = operand.z;
+	return *this;
+}
 
-    //Dot product
-    float Vector3::dotProd(const Vector3& operand)
-    {
-        return x * operand.x + operand.y * y + operand.z * z;
-    }
+//Dot product
+float Vector3::dotProd(const Vector3& operand)
+{
+	return x * operand.x + operand.y * y + operand.z * z;
+}
 
-    //Cross product
-    Vector3 Vector3::crossProd(const Vector3& operand)
-    {
-        return Vector3(y * operand.z - z * operand.y, 
-            z * operand.x - x * operand.z, x * operand.y - y * operand.x);
-    }
+//Cross product
+Vector3 Vector3::crossProd(const Vector3& operand)
+{
+	return Vector3(y * operand.z - z * operand.y,
+		z * operand.x - x * operand.z, x * operand.y - y * operand.x);
+}
 
-    //Normalize
-    Vector3 Vector3::normalized()
-    {
-        assert(magnitude() != 0);
-        *this /= magnitude();
-        return *this;
-    }
+//Normalize
+Vector3 Vector3::normalized()
+{
+	assert(magnitude() != 0);
+	*this /= magnitude();
+	return *this;
+}
 
-    
+
--- CGJ-Engine/AssignmentOne/src/main.cpp
+++ CGJ-Engine/AssignmentOne/src/main.cpp
@@ -2,55 +2,72 @@
 #include "../headers/Vector3.h"
 
 int main() {
-    Vector3 vec1(1,2,3);
-    std::cout << "Vector 1: " << vec1 << "\n";
-    std::cout << "  x: " << vec1.getX() << "\n";
-    std::cout << "  y: " << vec1.getY() << "\n";
-    std::cout << "  z: " << vec1.getZ() << "\n";
+	std::cout << "=======CONSTRUCTORS, SETTERS & GETTERS=======\n";
 
-    Vector3 vec2;
-    std::cout << "Vector 2: " << vec2 << "\n";
-    vec2.setX(-3);
-    vec2.setY(2);
-    vec2.setZ(5);
+	Vector3 vec1(1, 2, 3);
+	std::cout << "Vector 1: " << vec1 << "\n";
+	std::cout << "  x: " << vec1.getX() << "\n";
+	std::cout << "  y: " << vec1.getY() << "\n";
+	std::cout << "  z: " << vec1.getZ() << "\n";
 
-    std::cout << "Vector 2 after set:\n";
-    std::cout << "  x: " << vec2.getX() << "\n";
-    std::cout << "  y: " << vec2.getY() << "\n";
-    std::cout << "  z: " << vec2.getZ() << "\n";
+	Vector3 vec2;
+	std::cout << "Vector 2: " << vec2 << "\n";
+	vec2.setX(-3);
+	vec2.setY(2);
+	vec2.setZ(5);
+
+	std::cout << "Vector 2 after set:\n";
+	std::cout << "  x: " << vec2.getX() << "\n";
+	std::cout << "  y: " << vec2.getY() << "\n";
+	std::cout << "  z: " << vec2.getZ() << "\n";
+
+	std::cout << "\n=======MAGNITUDE=======\n";
 
 
-    std::cout << "Vector1's Magnitude: " << vec1.magnitude() << "\n";
-    std::cout << "Vector2's Magnitude: " << vec2.magnitude() << "\n";
+	std::cout << "Vector1's Magnitude: " << vec1.magnitude() << "\n";
+	std::cout << "Vector2's Magnitude: " << vec2.magnitude() << "\n";
 
-    std::cout << "Vector1 + Vector2 = " << vec1+vec2 << "\n";
+	std::cout << "\n=======VECTOR-VECTOR OPERATORS=======\n";
+	std::cout << "Vector1 + Vector2 = " << vec1 + vec2 << "\n";
 
-    Vector3 test = vec1 += vec2;
-    std::cout << "Vector1 += Vector2 = " << test << "\n";
+	std::cout << "Vector1 - Vector2 = " << vec1 - vec2 << "\n";
 
-    std::cout << "Vector1 - Vector2 = " << vec1 - vec2 << "\n";
+	std::cout << "\n=======VECTOR-SCALAR OPERATORS=======\n";
+	std::cout << "Vector1 * 2.0f = " << vec1 * 2.0f << "\n";
 
-    test = vec1 -= vec2;
-    std::cout << "Vector1 -= Vector2 = " << test << "\n";
+	std::cout << "Vector1 / 2.0f = " << vec1 / 2.0f << "\n";
 
-    std::cout << "Vector1 * 2.0f = " << vec1 * 2.0f << "\n";
+	std::cout << "\n=======ASSIGNMENTS=======\n";
+	Vector3 vec3(1, 1, 1);
+	std::cout << "Vector 3: " << vec3 << "\n";
+	vec3 += vec1;
+	std::cout << "Vector3 += Vector1 = " << vec3 << "\n";
 
-    test = vec1 *= 2.0f;
-    std::cout << "Vector1 *= 2.0f = " << test << "\n";
+	std::cout << "Vector 3: " << vec3 << "\n";
 
-    std::cout << "Vector1 / 2.0f = " << vec1 / 2.0f << "\n";
+	vec3 -= vec1;
+	std::cout << "Vector3 -= Vector1 = " << vec3 << "\n";
 
-    test = vec1 /= 2.0f;
-    std::cout << "Vector1 /= 2.0f = " << test << "\n";
+	std::cout << "Vector 3: " << vec3 << "\n";
 
-    test = vec1 = vec2;
-    std::cout << "Vector1 = Vector2 = " << test << "\n";
+	vec3 *= 2.0f;
+	std::cout << "Vector3 *= 2.0f = " << vec3 << "\n";
 
-    std::cout << "Vector1 dot product with Vector2 = " << vec1.dotProd(vec2) << "\n";
+	std::cout << "Vector 3: " << vec3 << "\n";
 
-    std::cout << "Vector1 cross product with Vector2 = " << vec1.crossProd(vec2) << "\n";
+	vec3 /= 2.0f;
+	std::cout << "Vector3 /= 2.0f = " << vec3 << "\n";
 
-    std::cout << "Vector1  Normalized" << vec1.normalized() << "\n";
+	vec3 = vec1;
+	std::cout << "Vector3 = Vector1 = " << vec3 << "\n";
 
-    return 0;
+	std::cout << "\n=======VECTOR METHODS=======\n";
+
+	std::cout << "Vector1 dot product with Vector2 = " << vec1.dotProd(vec2) << "\n";
+
+	std::cout << "Vector1 cross product with Vector2 = " << vec1.crossProd(vec2) << "\n";
+
+	std::cout << "Vector1  Normalized" << vec1.normalized() << "\n";
+
+	return 0;
 }
