57bd734103ce663198056088e06be112e6e52c81
HerouFenix <diogo04@ua.pt>
2020-11-17 10:57:43 +0000

    another small bug fix

--- CGJ-Engine/6_AssignmentSix/src/utils/scene/Scene.cpp
+++ CGJ-Engine/6_AssignmentSix/src/utils/scene/Scene.cpp
@@ -29,32 +29,32 @@ std::vector<SceneGraph*> Scene::GetSceneGraphs()
 void Scene::SetupTetrominoSceneGraph(SceneGraph* sceneGraph,Mesh* m, Shader* s)
 {
 	//Floor
-	//SceneNode* root = sceneGraph.AddNode(&mesh, &shader, NULL, Vector3(1, 0.01, 1));
+	//SceneNode* root = sceneGraph->AddNode(m, s, NULL, Vector3(1, 0.01, 1));
 	//root->ApplyLocalTransform(Matrix4::translation(0, -0.44, 0));
 	//
 	//Tetromino_SQ sq;
-	//SceneNode* square = sceneGraph.AddNode(&mesh, &shader, sq, Vector3(0.1, 0.1, 0.1));
+	//SceneNode* square = sceneGraph->AddNode(m, s, sq, Vector3(0.1, 0.1, 0.1));
 	//square->ApplyLocalTransform(Matrix4::translation(-0.11, 0.33, 0));
 	//
 	//Tetromino_L l;
-	//SceneNode* letterL = sceneGraph.AddNode(&mesh, &shader, l, Vector3(0.1, 0.1, 0.1));
+	//SceneNode* letterL = sceneGraph->AddNode(m, s, l, Vector3(0.1, 0.1, 0.1));
 	//letterL->ApplyLocalTransform(Matrix4::translation(-0.33, 0.11, 0));
 	//
 	//Tetromino_RL rl;
-	//SceneNode* letterRL = sceneGraph.AddNode(&mesh, &shader, rl, Vector3(0.1, 0.1, 0.1));
+	//SceneNode* letterRL = sceneGraph->AddNode(m, s, rl, Vector3(0.1, 0.1, 0.1));
 	//letterRL->ApplyLocalTransform(Matrix4::translation(0.33, 0.11, 0));
 	//
 	//Tetromino_I i;
-	//SceneNode* letterI = sceneGraph.AddNode(&mesh, &shader, i, Vector3(0.1, 0.1, 0.1));
+	//SceneNode* letterI = sceneGraph->AddNode(m, s, i, Vector3(0.1, 0.1, 0.1));
 	//letterI->ApplyLocalTransform(Matrix4::rotationZ(90, false, true));
 	//letterI->ApplyLocalTransform(Matrix4::translation(0.33, 0.77, 0));
 
 	//Floor
 	SceneNode* root = sceneGraph->AddNode(m, s, NULL, Vector3(1, 0.01, 1));
 	root->ApplyLocalTransform(Matrix4::translation(0, -0.44, 0));
-
+	
 	float colour[4];
-
+	
 	//Square
 	SceneNode* squareBase = sceneGraph->AddNode(m, s, NULL, Vector3(0.1, 0.1, 0.1));
 	squareBase->GetColour(colour);
@@ -65,7 +65,7 @@ void Scene::SetupTetrominoSceneGraph(SceneGraph* sceneGraph,Mesh* m, Shader* s)
 	squareThree->ApplyLocalTransform(Matrix4::translation(0.22, 0.22, 0));
 	SceneNode* squareFour = sceneGraph->AddNode(m,s, squareBase, colour, Vector3(0.1, 0.1, 0.1));
 	squareFour->ApplyLocalTransform(Matrix4::translation(0.22, 0, 0));
-
+	
 	// L
 	SceneNode* lBase = sceneGraph->AddNode(m, s, NULL, Vector3(0.1, 0.1, 0.1));
 	lBase->GetColour(colour);
@@ -76,7 +76,7 @@ void Scene::SetupTetrominoSceneGraph(SceneGraph* sceneGraph,Mesh* m, Shader* s)
 	lThree->ApplyLocalTransform(Matrix4::translation(0, 0.44, 0));
 	SceneNode* lFour = sceneGraph->AddNode(m, s, lBase, colour, Vector3(0.1, 0.1, 0.1));
 	lFour->ApplyLocalTransform(Matrix4::translation(0.22, 0, 0));
-
+	
 	// Reverse L
 	SceneNode* rlBase = sceneGraph->AddNode(m, s, NULL, Vector3(0.1, 0.1, 0.1));
 	rlBase->GetColour(colour);
@@ -87,13 +87,13 @@ void Scene::SetupTetrominoSceneGraph(SceneGraph* sceneGraph,Mesh* m, Shader* s)
 	rlThree->ApplyLocalTransform(Matrix4::translation(0, 0.44, 0));
 	SceneNode* rlFour = sceneGraph->AddNode(m, s, rlBase, colour, Vector3(0.1, 0.1, 0.1));
 	rlFour->ApplyLocalTransform(Matrix4::translation(-0.22, 0, 0));
-
+	
 	// I
 	SceneNode* iBase = sceneGraph->AddNode(m, s, NULL, Vector3(0.1, 0.1, 0.1));
 	iBase->GetColour(colour);
 	iBase->ApplyLocalTransform(Matrix4::rotationZ(90, false, true));
 	iBase->ApplyLocalTransform(Matrix4::translation(0.33, 0.88, 0));
-
+	
 	SceneNode* iTwo = sceneGraph->AddNode(m, s, iBase, colour, Vector3(0.1, 0.1, 0.1));
 	iTwo->ApplyLocalTransform(Matrix4::translation(0, 0.22, 0));
 	SceneNode* iThree = sceneGraph->AddNode(m, s, iBase, colour, Vector3(0.1, 0.1, 0.1));
